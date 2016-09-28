026f54c51780fbd914e097da059e26da1c0510fe
Stefan Will <swill@zendesk.com>
2016-09-28 12:07:39 -0400

    Use bloomfilter for deduping

--- src/main/java/stewi/MergeJob.java
+++ src/main/java/stewi/MergeJob.java
@@ -6,8 +6,10 @@ import java.util.Iterator;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MD5Hash;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
@@ -22,27 +24,51 @@ import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.hadoop.mapred.lib.LazyOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.util.bloom.DynamicBloomFilter;
+import org.apache.hadoop.util.bloom.Key;
+import org.apache.hadoop.util.hash.Hash;
+
+import com.google.common.hash.BloomFilter;
 
 import stewi.mapred.LenientSequenceFileInputFormat;
 
 public class MergeJob extends Configured implements Tool {
 
-    public static class MergeMapper extends MapReduceBase implements Mapper<LongWritable, Text, MD5Hash, PairWritable> {
-        @Override
-        public void map(LongWritable key, Text value, OutputCollector<MD5Hash, PairWritable> output, Reporter reporter)
-                throws IOException {
-            output.collect(
-                    new MD5Hash().digest(key.toString()).digest(value.toString()),
-                    new PairWritable(key,value));
-        }
-    }
+    public static class MergeReducer extends MapReduceBase implements Reducer<LongWritable, Text, LongWritable, Text> {
 
-    public static class MergeReducer extends MapReduceBase implements Reducer<MD5Hash, PairWritable, LongWritable, Text> {
+        Configuration conf;
+
         @Override
-        public void reduce(MD5Hash key, Iterator<PairWritable> values, OutputCollector<LongWritable, Text> output,
+        public void configure(JobConf job) {
+            conf = job;
+        }
+
+        @Override
+        public void reduce(LongWritable key, Iterator<Text> values, OutputCollector<LongWritable, Text> output,
                 Reporter reporter) throws IOException {
-            PairWritable pair = values.next();
-            output.collect(pair.getKey(), pair.getValue());
+            int n_keys = conf.getInt("mergejob.bloom.size", 1024 * 1024);
+            // Copied from BloomMapFile.java:
+            // vector size should be <code>-kn / (ln(1 - c^(1/k)))</code> bits for
+            // single key, where <code> is the number of hash functions,
+            // <code>n</code> is the number of keys and <code>c</code> is the desired
+            // max. error rate.
+            // Our desired error rate is by default 0.005, i.e. 0.5%
+            float errorRate = conf.getFloat("mergejob.bloom.error.rate", 0.005f);
+            int hash_count = 5;
+            int vectorSize = (int)Math.ceil((double)(-hash_count * n_keys) /
+                Math.log(1.0 - Math.pow(errorRate, 1.0/hash_count)));
+
+            DynamicBloomFilter filter = new DynamicBloomFilter(
+                    vectorSize, hash_count, Hash.getHashType(conf), n_keys);
+
+            while(values.hasNext()) {
+                Text line = values.next();
+                Key filterkey = new Key(line.copyBytes());
+                if(!filter.membershipTest(filterkey)) {
+                    output.collect(key,line);
+                    filter.add(filterkey);
+                }
+            }
         }
     }
 
@@ -71,14 +97,11 @@ public class MergeJob extends Configured implements Tool {
         FileInputFormat.addInputPath(job, new Path(args[0]) );
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setMapperClass(MergeJob.MergeMapper.class);
         job.setReducerClass(MergeJob.MergeReducer.class);
 
         job.setInputFormat(LenientSequenceFileInputFormat.class);
         MergeJobOutputFormat.setOutputFormatClass(job, SequenceFileOutputFormat.class);
 
-        job.setMapOutputKeyClass(MD5Hash.class);
-        job.setMapOutputValueClass(PairWritable.class);
         job.setOutputKeyClass(LongWritable.class);
         job.setOutputValueClass(Text.class);
 
--- src/main/java/stewi/PairWritable.java
+++ src/main/java/stewi/PairWritable.java
@@ -1,13 +1,14 @@
 package stewi;
 
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-class PairWritable implements Writable {
+class PairWritable implements WritableComparable<PairWritable> {
     private LongWritable key = new LongWritable();
     private Text value = new Text();
 
@@ -34,6 +35,21 @@ class PairWritable implements Writable {
     public void readFields(DataInput in) throws IOException {
         key.readFields(in);
         value.readFields(in);
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode() + 31*value.hashCode();
+    }
+
+    @Override
+    public int compareTo(PairWritable o) {
+        // Primary sort on key, secondary sort on value
+        if(key.equals(o.key)) {
+            return value.compareTo(o.value);
+        } else {
+            return key.compareTo(o.key);
+        }
     }
 }
 
