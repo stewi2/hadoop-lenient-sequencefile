ef39ba83ec1b806698cb047f22d345bd1545cfbd
Stefan Will <swill@zendesk.com>
2016-09-28 16:02:38 +0000

    fix outputfile naming

--- src/main/java/stewi/MergeJobOutputFormat.java
+++ src/main/java/stewi/MergeJobOutputFormat.java
@@ -1,32 +1,35 @@
 package stewi;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.TreeMap;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionCodecFactory;
 import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.OutputFormat;
 import org.apache.hadoop.mapred.RecordWriter;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.lib.FilterOutputFormat;
 import org.apache.hadoop.mapred.lib.LazyOutputFormat;
-import org.apache.hadoop.mapred.lib.MultipleSequenceFileOutputFormat;
 import org.apache.hadoop.util.Progressable;
 import org.apache.hadoop.util.ReflectionUtils;
 
 public class MergeJobOutputFormat<K, V> extends LazyOutputFormat<K, V> {
 
+    @SuppressWarnings("unchecked")
+    public static void  setOutputFormatClass(JobConf job, 
+        Class<? extends OutputFormat> theClass) {
+        job.setOutputFormat(MergeJobOutputFormat.class);
+        job.setClass("mapreduce.output.lazyoutputformat.outputformat", theClass, OutputFormat.class);
+    }
+
     @Override
     public RecordWriter<K, V> getRecordWriter(FileSystem ignored, JobConf job, String name, Progressable progress)
             throws IOException {
+        String thename = name;
         if(name.startsWith("part")) {
             Class<? extends CompressionCodec> codecClass = FileOutputFormat.getOutputCompressorClass(job,DefaultCodec.class);
             CompressionCodec codec = ReflectionUtils.newInstance(codecClass, job);
-            name = "merged_help_center_" + name.substring(4) + codec.getDefaultExtension();
+            name = "merged_help_center" + name.substring(4) + codec.getDefaultExtension();
         }
         return super.getRecordWriter(ignored, job, name, progress);
     }
